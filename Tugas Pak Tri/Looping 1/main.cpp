ce84d2c9671e873fc57064449f71ab380351c6b4
Bangkit Haqi Aliaffuan <uwawuwuz@gmail.com>
2024-09-29 14:54:40 +0700

    selesai

---
+++ Tugas Pak Tri/Looping 1/main.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int i = 1;  i <= 4; i++)
+    {
+        cout << "ini adalah Angka";
+        cout << endl;
+        for (int j = 1; j <= 4; j++)
+        {
+        cout << i << "." << j;
+        std::cout << endl;
+        }
+        std::cout << endl;
+    }
+    return 0;
+}
---
+++ Tugas Pak Tri/Looping 2/main.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Perulangan For" << endl;
+    for (int i = 1; i <= 3; ++i)
+    for (int j = 1; j <= 5; ++j)
+        {
+            std::cout << "ini i " << i << " ini j " << j << " ini adalah i j: " << i << "." << j;
+            std::cout << endl;
+        }
+
+    return 0;
+}
