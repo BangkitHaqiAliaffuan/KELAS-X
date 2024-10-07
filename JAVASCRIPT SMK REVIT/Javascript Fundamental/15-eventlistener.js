611194aac1667998089bccb23fc7be4ff0e6eecd
Bangkit Haqi Aliaffuan <uwawuwuz@gmail.com>
2024-10-07 13:04:56 +0700

    Add files via upload

---
+++ JAVASCRIPT SMK REVIT/Javascript Fundamental/15-eventlistener.js
@@ -0,0 +1,12 @@
+
+function coba(){
+    a = document.querySelector(".isi");
+    a.innerHTML = "Belajar Event JS";
+    console.log("Coba EventListener");
+}
+
+
+
+judul.onmouseover = function(){
+    console.log("Coba Function Anonymous");
+}
